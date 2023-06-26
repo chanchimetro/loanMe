21885d8f05fb68e2cf70e6f7bc7e47143201bdbb
chanchimetro <48773442+chanchimetro@users.noreply.github.com>
2023-06-25 21:44:30 -0300

    added form carousel WIP

--- src/register/Register.js
+++ src/register/Register.js
@@ -26,31 +26,44 @@ function Register() {
         <div className="Register">
             <h2 className="card-title">Register</h2>
             <hr></hr>
-            <div className="card-body text-start py-md-4">
-                <form onSubmit={(e) => handleChanges(e)}>
-                    <div className="mb-3">
-                        <label className="form-label">Username</label>
-                        <input type="text" className="form-control" id="username" />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label">Password</label>
-                        <input type="password" className="form-control" id="password" />
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Tipo de usuario</label>
-                        <select id="tipoUsuario" class="form-select">
-                            <option selected>Seleccione un tipo de usuario</option>
-                            <option value="Prestatario">Prestatario</option>
-                            <option value="Prestamista">Prestamista</option>
-                        </select>
-                    </div>
-                    <button type="submit" className="btn btn-primary float-end">Submit</button>
-                </form>
+            <div className="card-body text-start p -md-4">
+                <div id="carouselExampleIndicators" className="carousel carousel-dark slide">
+                    <form onSubmit={(e) => handleChanges(e)}>
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <div className="mb-3">
+                                    <label className="form-label">Username</label>
+                                    <input type="text" className="form-control" id="username" />
+                                    <div className="mb-3">
+                                        <label className="form-label">Email address</label>
+                                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+                                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label className="form-label">Password</label>
+                                        <input type="password" className="form-control" id="password" />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label className="form-label">Tipo de usuario</label>
+                                        <select id="tipoUsuario" className="form-select">
+                                            <option selected>Seleccione un tipo de usuario</option>
+                                            <option value="Prestatario">Prestatario</option>
+                                            <option value="Prestamista">Prestamista</option>
+                                        </select>
+                                    </div>
+                                    <button className="btn btn-link float-end" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                        Next →
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="carousel-item">
+                            </div>
+                            <div className="carousel-item">
+                                <button type="submit" className="btn btn-primary float-end">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
