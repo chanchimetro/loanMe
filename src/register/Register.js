f2075b02ad37ffcc9a373e01bb21895d4d12d5b7
chanchimetro <48773442+chanchimetro@users.noreply.github.com>
2023-06-25 22:44:28 -0300

    fix

--- src/register/Register.js
+++ src/register/Register.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import "./Register.css";
 import axios from 'axios';
 
 let url = "https://10.152.2.102:4433/api/auth/registro";
+
 
 async function postData(url = "", data = {}) {
     const resp = axios.post(url, data);
@@ -9,6 +11,7 @@ async function postData(url = "", data = {}) {
 }
 
 function Register() {
+    const [prestamista, setPrestamista] = useState(false);
 
     function handleChanges(e) {
         e.preventDefault();
@@ -34,29 +37,45 @@ function Register() {
                                 <div className="mb-3">
                                     <label className="form-label">Username</label>
                                     <input type="text" className="form-control" id="username" />
-                                    <div className="mb-3">
-                                        <label className="form-label">Email address</label>
-                                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
-                                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                                    </div>
-                                    <div className="mb-3">
-                                        <label className="form-label">Password</label>
-                                        <input type="password" className="form-control" id="password" />
-                                    </div>
-                                    <div className="mb-3">
-                                        <label className="form-label">Tipo de usuario</label>
-                                        <select id="tipoUsuario" className="form-select">
-                                            <option selected>Seleccione un tipo de usuario</option>
-                                            <option value="Prestatario">Prestatario</option>
-                                            <option value="Prestamista">Prestamista</option>
-                                        </select>
-                                    </div>
-                                    <button className="btn btn-link float-end" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                                        Next →
-                                    </button>
                                 </div>
+                                <div className="mb-3">
+                                    <label className="form-label">Email address</label>
+                                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+                                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                                </div>
+                                <div className="mb-3">
+                                    <label className="form-label">Password</label>
+                                    <input type="password" className="form-control" id="password" />
+                                </div>
+                                <div className="mb-3">
+                                    <label className="form-label">Tipo de usuario</label>
+                                    <select id="tipoUsuario" className="form-select">
+                                        <option selected>Seleccione un tipo de usuario</option>
+                                        <option value="Prestatario" onclick={()=>setPrestamista(false)}>Prestatario</option>
+                                        <option value="Prestamista" onClick={()=>setPrestamista(true)}>Prestamista</option>
+                                    </select>
+                                </div>
+                                <button className="btn btn-link float-end" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                    Next →
+                                </button>
                             </div>
+                            {prestamista ? 
                             <div className="carousel-item">
+                                <div>prestamista si</div>
+                                <button className="btn btn-link float-end" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                    Next →
+                                </button>
+                            </div>
+                            : 
+                            <div className="carousel-item">
+                            <button className="btn btn-link float-end" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                    Next →
+                                </button>
+                            </div>
+
+                            }
+                            <div className="carousel-item">
+
                             </div>
                             <div className="carousel-item">
                                 <button type="submit" className="btn btn-primary float-end">Submit</button>
